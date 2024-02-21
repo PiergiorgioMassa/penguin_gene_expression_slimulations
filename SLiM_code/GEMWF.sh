30542c40869e8906894260724dcf9dad0cd6beed
PiergiorgioMassa <160726978+PiergiorgioMassa@users.noreply.github.com>
2024-02-21 12:14:14 +0100

    Add files via upload

---
+++ SLiM_code/GEMWF.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+OUT_DIR=/PATH/TO/OUT/DIR #set output directory
+K=1000 #set carrying capacity
+
+cd $OUT_DIR
+for SEED in {1..3} #set number of replicates
+do
+slim -d seed=$SEED -d K=$K GEMWF.slim &> GEMWF.seed${SEED}.oe &
+done
---
+++ SLiM_code/PSMWF.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+OUT_DIR=/PATH/TO/OUT/DIR #set output directory
+K=1000 #set carrying capacity
+
+cd $OUT_DIR
+for SEED in {1..3} #set number of replicates
+do
+slim -d seed=$SEED -d K=$K PSMWF.slim &> PSMWF.seed${SEED}.oe &
+done
